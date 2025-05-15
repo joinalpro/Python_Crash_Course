98dad883e6f54f306a4667adb7e1a98a2f582fce
joinalzx <joinalzx@gmail.com>
2025-05-15 10:46:49 +0600

    if statement

--- chapter-five/Test/5-1-conditional-tests.py
+++ chapter-five/Test/5-1-conditional-tests.py
@@ -1,6 +1,19 @@
-car = 'subaru'
-print("Is car == 'subaru'? I predict True.")
-print(car == 'subaru')
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
 
-print("\nIs car == 'audi'? I predict False.")
-print( car == 'audi')
+# print("\nIs car == 'audi'? I predict False.")
+# print( car == 'audi')
+
+# my_cars = ['audi','bmw','prado']
+# new_car = 'prado'
+# if new_car in my_cars:
+#     print(f"{new_car.title()} is in your list")
+
+# car = 'premio'
+# print("Is car == 'premio'? I predict True.")
+# # print(car == 'subaru')
+# print(car == 'premio')
+car = 'Premio'
+# print(car == 'premio')
+print(car.lower() == 'premio')
---
+++ chapter-five/voting.py
@@ -0,0 +1,6 @@
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("You are too young to vote.")
