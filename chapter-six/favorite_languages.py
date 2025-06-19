b4b8b83a4f7385174f0ca23ef102bdfa5f659149
joinalzx <joinalzx@gmail.com>
2025-06-19 19:36:38 +0600

    dictionaries

--- chapter-six/favorite_languages.py
+++ chapter-six/favorite_languages.py
@@ -43,3 +43,14 @@ print("The following languages hav been mentioned:")
 
 languages = {'python', 'ruby', 'python', 'c'}
 print(languages)
+
+favorite_languages = {
+ 'jen': ['python', 'ruby'],
+ 'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are: ")
+    for language in languages:
+        print(f"\t{language.title()}")
